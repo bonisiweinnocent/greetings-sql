8f1ffa41ec48db9302c57b44726a54c2009aac62
bonisiweinnocent <bonisiwe.cukatha@gmail.com>
2021-08-25 10:24:44 +0200

    greet app

---
+++ index.js
@@ -0,0 +1,104 @@
+// define my variables
+const flash = require('express-flash');
+const session = require('express-session');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser')
+const greet = require('./script');
+const app = express();
+const greetApp = greet();
+// const moment =require('moment')
+// moment().format();
+
+
+app.use(express.static('public'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main', layoutsDir: __dirname + '/views/layouts' }));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+
+    res.render('index', {
+         greets: greetApp.getMsg(),
+        // counterRender: greetApp.getName()
+
+
+    })
+
+})
+
+
+// initialise session middleware - flash-express depends on it
+app.use(session({
+    secret: "<add a secret string here>",
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(flash());
+
+
+app.post('/greet', function (req, res) {
+    var msg = ""
+    const enterName = req.body.enterName;
+    const language = req.body.language
+    if (enterName === "" && !language) {
+        req.flash('info', 'Please type in your name and select a language.');
+    }
+    else {
+        if (enterName && language) {
+            greetApp.greetings(req.body.language, req.body.enterName);
+            greetApp.store(enterName)
+            msg = greetApp.getMsg()
+
+        }
+    }
+
+    res.render('index', { msg, counter : greetApp() })
+});
+
+
+app.post('index', function (req, res) {
+
+
+
+});
+
+app.post('/greetings', (req, res) => {
+
+
+    res.render('greetings', { namesGreeted: greetApp.storeArray() })
+
+
+    console.log(typeof namesGreeted)
+})
+
+
+app.get('/counter/:enterName', (req, res)=>{
+var name = req.params.enterName 
+var namesList = greetApp.storeArray()
+ 
+res.render('counter', {
+    name: name,
+    personsCounter : namesList[name]
+})
+
+})
+
+
+
+
+
+2
+
+
+
+const PORT = process.env.PORT || 3013;
+
+app.listen(PORT, function () {
+    console.log("app started ")
+});
---
+++ script.js
@@ -0,0 +1,98 @@
+module.exports = function greet(storage) {
+
+
+    var msg = "";
+    var nameStore = {};
+
+    function greetings(message, param) {
+
+        var param1 = param.charAt(0).toUpperCase() + param.slice(1).toLowerCase();
+        if (message == "Swahili") {
+            msg = 'Jambo, ' + param1;
+
+        } else if
+            (message == "French") {
+            msg = "Bonjour, " + param1;
+
+        } else if
+            (message == "IsiXhosa") {
+            msg = "Molo, " + param1;
+        }
+    }
+    function errorSpecial() {
+
+        return "Please type only letters."
+    }
+
+    function getMsg() {
+        return msg
+    }
+    function bothError(name, language) {
+        if (name === "" && !language) {
+            return "Please type in your name and select a language."
+
+        }
+    }
+
+
+    function errorsNoName(name) {
+        if (!name) {
+            return "Please type in your name below."
+
+        }
+    }
+
+    function languageErrors(lang) {
+        if (!lang) {
+            return "Please select a language."
+
+        }
+    }
+    function storageError(click) {
+        if (click) {
+            return "You have successfully cleared your local storage"
+
+        }
+    }
+    function store(names) {
+        if(nameStore[names] === undefined){
+            nameStore[names] =  1
+        }
+        else {
+            nameStore[names]++
+        }
+        // var nameTake = names.charAt(0).toUpperCase() + names.slice(1).toLowerCase();
+        // if (!nameStore.includes(nameTake)) {
+        //     nameStore.push(nameTake)
+        //     return true
+        // }
+    }
+    function countNames() {
+        var objNames = Object.keys(nameStore)
+        return objNames.length
+    }
+    function timer() {
+        return "";
+    }
+    function storeArray() {
+        return nameStore
+    }
+
+
+
+    return {
+        greetings,
+        getMsg,
+        errorsNoName,
+        languageErrors,
+        bothError,
+        errorSpecial,
+        store,
+        countNames,
+        timer,
+        storeArray,
+        storageError
+
+    }
+
+}
